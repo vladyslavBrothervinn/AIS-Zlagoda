feb6e58951b37bc67252aa60c1beff918de9ad2d
AMykolaD <andrusenkomykola@gmail.com>
2023-06-18 09:56:25 +0300

    CheckSales

--- src/main/java/com/example/demo/HelloTableController.java
+++ src/main/java/com/example/demo/HelloTableController.java
@@ -1,34 +1,23 @@
 package com.example.demo;
 
-import db.DatabaseManager;
-import instruments.StoreProductManager;
-import instruments.TableManager;
+import instruments.CheckSalesTableManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
-import models.Employee;
-import models.Store_Product;
 
-import java.sql.Date;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class HelloTableController {
     @FXML
-    TableView<Employee> tableView;
-    TableManager<Employee> tableManager;
-    StoreProductManager storeProductManager;
+    TableView tableView;
+    CheckSalesTableManager checkSalesTableManager;
+
     boolean x = false;
-    public void initialize() throws SQLException, ClassNotFoundException {
-        tableManager = new TableManager<>(DatabaseManager.getDatabaseManager(), tableView, "Employee");
+    public void initialize() throws SQLException {
+        checkSalesTableManager = new CheckSalesTableManager(tableView, "241455");
     }
 
     public void refreshTable() throws SQLException {
-        ResultSet r = DatabaseManager.getDatabaseManager().selectRecords("Employee");
-        r.next();
-        if(!x) tableManager.updateRow(new String[]{r.getString(1)},
-                new Employee("AAAAAAAAAAAA", "Стефаненко", "Стефан", "Стефанович",
-                        "Касир", 10000.0, new Date(120,1,1), new Date(120, 1, 1),
-                        "091201234", "Івано-Франківськ", "Франка", "02102"));
+        checkSalesTableManager.renewTable();
         x = !x;
         //x = !x;
         //if(x)
